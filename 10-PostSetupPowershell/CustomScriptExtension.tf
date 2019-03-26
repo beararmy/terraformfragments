99d16fa1818098ad66ea52b900da3a2ab37747d0
Stefan Harrington-Palmer <stef@bear.army>
2019-03-26 14:55:45 +0000

    Create codeblock to use CustomScriptExtension

---
+++ 10-PostSetupPowershell/CustomScriptExtension.tf
@@ -0,0 +1,16 @@
+resource "azurerm_virtual_machine_extension" "CustomExtension" {
+  name                 = "${azurerm_virtual_machine.vm.name}-CustomScriptExtension"
+  location             = "${var.location}"
+  resource_group_name  = "${var.naming_prefix}-rg"
+  virtual_machine_name = "${azurerm_virtual_machine.vm.name}"
+  type                 = "CustomScriptExtension"
+  publisher            = "Microsoft.Compute"
+  type_handler_version = "1.9"
+
+  settings = <<SETTINGS
+        {   
+        "fileUris": [ "https://github.com/folder/script.ps1" ],
+        "commandToExecute": "powershell.exe -ExecutionPolicy Unrestricted -File script.ps1"
+        }
+SETTINGS
+}
---
+++ 10-PostSetupPowershell/variables.tf
@@ -0,0 +1,8 @@
+variable "naming_prefix" {
+  description = "The prefix which should be used for all resources in this example."
+  default = "defaultprefix"
+}
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default = "uksouth"
+}
